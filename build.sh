4ba8da353762a1bed74f9493c411c25b60f69a5f
RoboStore <robostorepl@gmail.com>
2026-09-16 16:18:14 +0200

    Stop seeding demo data on deploy

--- build.sh
+++ build.sh
@@ -5,6 +5,5 @@ set -o errexit
 pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py migrate
-python manage.py seed_demo
 python manage.py create_demo_user
 python manage.py create_initial_admin
